233c0de6f4b2e2bde653ed8f403b15a1866035c0
Gary Roulstone <47697948+grouls@users.noreply.github.com>
2020-10-15 11:24:31 +0100

    add todos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import "./styles.css";
 function App() {
   const [names, setNames] = useLocalStorageState("names", []);
   const handleSubmit = (event) => {
+    /*
+    todo:
+     * validate for dupilicates
+     * clear input as name is added
+     * show counter, e.g. 2/10 names added
+    */
     event.preventDefault();
     const namesCopy = [...names];
     namesCopy.push(event.target.elements.name.value);
     setNames(namesCopy);
   };
+  /*
+   todo:
+    * disable pick/reset buttons when no names
+    * disable plus button when list is full
+    * style whole app & display selected name better than alert
+   */
   const pickName = () => {
     alert(names[Math.floor(Math.random() * names.length)]);
   };
-  console.log(window.localStorage);
+
+  /* 
+  todo: 
+    * re-render list when local storage is cleared
+  */
+  const reset = () => window.localStorage.removeItem("names");
+  /*
+  todo:
+    * make reusable components
+  */
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
@@ -23,7 +44,7 @@ function App() {
       </form>
       <div>
         <button onClick={pickName}>pick</button>
-        <button onClick={() => setNames([])}>reset</button>
+        <button onClick={reset}>reset</button>
       </div>
       {names && names.map((name) => <li>{name}</li>)}
     </div>
