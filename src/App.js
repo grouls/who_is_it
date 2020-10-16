6d8db3662c211dff0646e587d304a3c24ab7531b
Gary Roulstone <47697948+grouls@users.noreply.github.com>
2020-10-16 10:08:59 +0100

    add proper key to list items

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useLocalStorageState } from "./utils";
 import "./styles.css";
 
 function App() {
-  const [names, setNames] = useLocalStorageState("names", []);
+  const [people, setNames] = useLocalStorageState("people", []);
   const [count, setCount] = useLocalStorageState("count", 0);
   const handleSubmit = (event) => {
     event.preventDefault();
     const newName = event.target.elements.name.value;
-    const namesCopy = [...names];
-    if (namesCopy[namesCopy.length - 1] === newName) {
+    const peopleCopy = [...people];
+    if (peopleCopy[peopleCopy.length - 1] === newName) {
       /* return for now, proper validation needed */
       return;
     }
-    namesCopy.push(newName);
-    setNames(namesCopy);
-    setCount(namesCopy.length);
+    // namesCopy.push(newName);
+    peopleCopy.push({ id: people.length, name: newName });
+    setNames(peopleCopy);
     event.target.elements.name.value = "";
   };
   /*
@@ -24,7 +24,7 @@ function App() {
     * investigate entering csv names to populate names array
    */
   const pickName = () => {
-    alert(names[Math.floor(Math.random() * names.length)]);
+    alert(people[Math.floor(Math.random() * people.length)].name);
   };
 
   const reset = () => {
@@ -34,27 +34,26 @@ function App() {
   /*
   todo:
     * make reusable components
-    * get key for the list items
   */
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Enter name: </label>
         <input id="name" />
-        <button type="submit" disabled={names.length > 4 ? true : false}>
+        <button type="submit" disabled={people.length > 4 ? true : false}>
           +
         </button>
-        <p>{count}/5 names added.</p>
+        <p>{count}/5 people added.</p>
       </form>
       <div>
-        <button onClick={pickName} disabled={names.length <= 1 ? true : false}>
+        <button onClick={pickName} disabled={people.length <= 1 ? true : false}>
           pick
         </button>
-        <button onClick={reset} disabled={names.length === 0 ? true : false}>
+        <button onClick={reset} disabled={people.length === 0 ? true : false}>
           reset
         </button>
       </div>
-      {names && names.map((name) => <li>{name}</li>)}
+      {people && people.map(({ id, name }) => <li key={id}>{name}</li>)}
     </div>
   );
 }
